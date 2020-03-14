29aadad8b524be74a0e569567a7d20c9b9f33088
TheOctan <kanatush.tim@gmail.com>
2020-03-14 13:30:33 +0300

    forking player and first scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,18 +6,34 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
-    PlayerController controller;
+
+    private Camera viewCamera;
+    private PlayerController controller;
     
     void Start()
     {
         controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+
+        
     }
 
     void Update()
     {
-        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
 
         controller.Move(moveVelocity);
+
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if(groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin, point, Color.red);
+            controller.LookAt(point);
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,32 +1,37 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    private Vector3 velocity;
-    private Rigidbody rigidbodyComponent;
+	private Vector3 velocity;
+	private Rigidbody rigidbodyComponent;
 
-    void Start()
-    {
-        rigidbodyComponent = GetComponent<Rigidbody>();
-    }
+	void Start()
+	{
+		rigidbodyComponent = GetComponent<Rigidbody>();
+	}
 
-    void Update()
-    {
-        
-    }
+	void Update()
+	{
+		
+	}
 
-    void FixedUpdate()
-    {
-        rigidbodyComponent.MovePosition(velocity);
-    }
+	void FixedUpdate()
+	{
+		rigidbodyComponent.MovePosition(rigidbodyComponent.position + velocity * Time.fixedDeltaTime);
+	}
 
-    public void Move(Vector3 velocity)
-    {
-        this.velocity = velocity;
-    }
+	public void Move(Vector3 velocity)
+	{
+		this.velocity = velocity;
+	}
+
+	public void LookAt(Vector3 lookPoint)
+	{
+		Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+		transform.LookAt(heightCorrectedPoint);
+	}
 }
