082453e92da370370f8eef049d038660b0d1f472
TheOctan <kanatush.tim@gmail.com>
2020-03-15 21:32:33 +0300

    gun system

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public Transform weaponHold;
+    private Gun equippedGun;
+    public void EquipGun(Gun gunToEquip)
+    {
+        if (equippedGun != null)
+        {
+            Destroy(equippedGun.gameObject);
+        }
+
+        equippedGun = Instantiate(gunToEquip, weaponHold.position, weaponHold.rotation);
+        equippedGun.transform.parent = weaponHold;
+    }
+}
