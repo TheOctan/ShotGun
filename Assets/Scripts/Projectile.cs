934f0783dab30531055c5a81819c58320b8b8509
TheOctan <kanatush.tim@gmail.com>
2020-03-16 13:34:24 +0300

    bullet prefab

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private float speed = 10f;
+
+    public void SetSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
