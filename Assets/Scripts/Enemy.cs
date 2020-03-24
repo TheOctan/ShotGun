437f2e813d9ad1bab44c4437b3c091e6570f6176
TheOctan <kanatush.tim@gmail.com>
2020-03-24 18:52:02 +0300

    refresh rate of path finding

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent pathFinder;
+    private Transform target;
+
+    void Start()
+    {
+        pathFinder = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        StartCoroutine(UpdatePath());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator UpdatePath()
+    {
+        float refreshRate = 0.25f;
+
+        while (target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathFinder.SetDestination(targetPosition);
+
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
