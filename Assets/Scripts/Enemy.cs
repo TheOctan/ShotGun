e856498a818bf6f4ba243185c23c239e17b18624
TheOctan <kanatush.tim@gmail.com>
2020-03-26 08:39:36 +0300

    damageable player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -16,6 +16,7 @@ public class Enemy : LivingEntity
 
 	private NavMeshAgent pathFinder;
 	private Transform target;
+	private LivingEntity targetEntity;
 	private Material skinMaterial;
 
 	private Color originalColor;
@@ -27,6 +28,9 @@ public class Enemy : LivingEntity
 	private float myCollisionRadius;
 	private float targetCollisionRadius;
 
+	private bool hasTarget;
+	private float damage = 1;
+
 	protected override void Start()
 	{
 		base.Start();
@@ -34,25 +38,43 @@ public class Enemy : LivingEntity
 		skinMaterial = GetComponent<Renderer>().material;
 		originalColor = skinMaterial.color;
 
-		currentState = State.Chasing;
-		target = GameObject.FindGameObjectWithTag("Player").transform;
+		var player = GameObject.FindGameObjectWithTag("Player");
 
-		myCollisionRadius = GetComponent<CapsuleCollider>().radius;
-		targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
+		if(player != null)
+		{
+			currentState = State.Chasing;
+			hasTarget = true;
 
-		StartCoroutine(UpdatePath());
+			target = player.transform;
+			targetEntity = player.GetComponent<LivingEntity>();
+			targetEntity.OnDeath += OnTargetDeath;
+
+			myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+			targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
+
+			StartCoroutine(UpdatePath());
+		}		
+	}
+
+	void OnTargetDeath()
+	{
+		hasTarget = false;
+		currentState = State.Idle;
 	}
 
 	void Update()
 	{
-		if (Time.time > nextAttackTime)
+		if (hasTarget)
 		{
-			float sqrDistToTarget = (target.position - transform.position).sqrMagnitude;
-
-			if (sqrDistToTarget < Mathf.Pow(attackDistanceThreshold + myCollisionRadius + targetCollisionRadius, 2))
+			if (Time.time > nextAttackTime)
 			{
-				nextAttackTime = Time.time + timeBetweenAttacks;
-				StartCoroutine(Attack());
+				float sqrDistToTarget = (target.position - transform.position).sqrMagnitude;
+
+				if (sqrDistToTarget < Mathf.Pow(attackDistanceThreshold + myCollisionRadius + targetCollisionRadius, 2))
+				{
+					nextAttackTime = Time.time + timeBetweenAttacks;
+					StartCoroutine(Attack());
+				}
 			}
 		}
 	}
@@ -70,9 +92,16 @@ public class Enemy : LivingEntity
 		float percent = 0;
 
 		skinMaterial.color = Color.red;
+		bool hasAppliedDamage = false;
 
 		while (percent <= 1)
 		{
+			if(percent >= 0.5f && !hasAppliedDamage)
+			{
+				hasAppliedDamage = true;
+				targetEntity.TakeDamage(damage);
+			}
+
 			percent += Time.deltaTime * attackSpeed;
 			float interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
 			transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
@@ -89,7 +118,7 @@ public class Enemy : LivingEntity
 	{
 		float refreshRate = 0.25f;
 
-		while (target != null)
+		while (hasTarget)
 		{
 			if (currentState == State.Chasing)
 			{
--- Assets/Scripts/LivingEntity.cs
+++ Assets/Scripts/LivingEntity.cs
@@ -18,9 +18,15 @@ public class LivingEntity : MonoBehaviour, IDamageable
 
     public void TakeHit(float damage, RaycastHit hit)
     {
+        // Do some stuff here with hit
+        TakeDamage(damage);
+    }
+
+    public void TakeDamage(float damage)
+    {
         health -= damage;
-        
-        if(health <= 0 && !dead)
+
+        if (health <= 0 && !dead)
         {
             Die();
         }
