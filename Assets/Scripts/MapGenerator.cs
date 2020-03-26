d99f76582e579445183e84690a04c71659d1bc75
TheOctan <kanatush.tim@gmail.com>
2020-03-26 19:26:48 +0300

    test tile map

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+	public Transform tilePrefab;
+	public Vector2 mapSize;
+
+	void Start()
+	{
+		GenerateMap();
+	}
+
+	public void GenerateMap()
+	{
+		for (int x = 0; x < mapSize.x; x++)
+		{
+			for (int y = 0; y < mapSize.y; y++)
+			{
+				Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
+				Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90));
+
+
+			}
+		}
+	}
+}
