247650e720b002487b06fc58ffa4618ed0c92941
TheOctan <kanatush.tim@gmail.com>
2020-04-11 20:53:46 +0300

    main menu and settings states

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject mainMenuHolder;
+    public GameObject optionMenuHolder;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsMenu()
+    {
+        mainMenuHolder.SetActive(false);
+        optionMenuHolder.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        mainMenuHolder.SetActive(true);
+        optionMenuHolder.SetActive(false);
+    }
+
+    public void SetScreenResolution(int i)
+    {
+        
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+
+    }
+
+    public void SetMasterVolume(float value)
+    {
+
+    }
+
+    public void SetMusicVolume(float value)
+    {
+
+    }
+
+    public void SetSfxVolume(float value)
+    {
+
+    }
+}
