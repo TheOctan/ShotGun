a8176b1da5f99ad535aadcddb17aa769388719ea
TheOctan <kanatush.tim@gmail.com>
2020-04-13 12:13:45 +0300

    screen resolution toggles

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     public GameObject mainMenuHolder;
     public GameObject optionMenuHolder;
+
+    public Slider[] volumeSliders;
+    public Toggle[] resolutionToggles;
+    public int[] screenWidths;
+    private int activeScreenResIndex;
 
     public void Play()
     {
@@ -32,12 +38,31 @@ public class Menu : MonoBehaviour
 
     public void SetScreenResolution(int i)
     {
-        
+        if (resolutionToggles[i].isOn)
+        {
+            activeScreenResIndex = i;
+            float aspectRatio = 16 / 9f;
+            Screen.SetResolution(screenWidths[i], (int)(screenWidths[i] / aspectRatio), false);
+        }
     }
 
     public void SetFullscreen(bool isFullscreen)
     {
+        for (int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles[i].interactable = !isFullscreen;
+        }
 
+        if (isFullscreen)
+        {
+            Resolution[] allResolutions = Screen.resolutions;
+            Resolution maxResolution = allResolutions[allResolutions.Length - 1];
+            Screen.SetResolution(maxResolution.width, maxResolution.height, true);
+        }
+        else
+        {
+            SetScreenResolution(activeScreenResIndex);
+        }
     }
 
     public void SetMasterVolume(float value)
