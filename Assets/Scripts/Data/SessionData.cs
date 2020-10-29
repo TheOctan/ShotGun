60a628115e31a521a51202860efc7ab5019be892
TheOctan <kanatush.tim@gmail.com>
2020-10-29 15:51:06 +0300

    session data

---
+++ Assets/Scripts/Data/SessionData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Data
+{
+	public class SessionData
+	{
+		public int ShootCount { get; set; }
+		public int ReloadCount { get; set; }
+		public float TraveledDistance { get; set; }
+		public float Time { get; set; }
+		public string Date { get; set; }
+		public float Score { get; set; }
+		public float ShootDamage { get; set; }
+		public float HitDamage { get; set; }
+	}
+}
