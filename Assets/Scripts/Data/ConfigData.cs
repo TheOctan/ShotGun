f6c7b2dea7dea21f9a9c95131088f00d4cac9563
TheOctan <kanatush.tim@gmail.com>
2020-11-02 14:40:17 +0300

    config data

---
+++ Assets/Scripts/Data/ConfigData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Data
+{
+	public class ConfigData
+	{
+		public string Nickname { get; set; }
+		public float MasterVolume { get; set; }
+		public float MusicVolume { get; set; }
+		public float SoundVolume { get; set; }
+		public int ResolutionIndex { get; set; }
+		public bool IsFullScreen { get; set; }
+	}
+}
