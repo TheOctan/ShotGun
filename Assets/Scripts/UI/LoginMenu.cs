cbf78593690a468b5b6e191c6e85898c2b179f42
TheOctan <kanatush.tim@gmail.com>
2020-12-11 14:00:48 +0300

    test login menu

---
+++ Assets/Scripts/UI/LoginMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginMenu : MonoBehaviour
+{
+	[Header("Input")]
+	public InputField nickname;
+	public InputField password;
+
+	[Header("Sender"), SerializeField]
+	private BaseSender sender = null;
+
+	[Header("UI elements")]
+	public GameObject loadingWindow;
+	public Text prompt;
+
+	private void Awake()
+	{
+		password.asteriskChar = '•';
+	}
+
+	public void OnLogin()
+	{
+		loadingWindow.SetActive(true);
+
+		StartCoroutine(sender.Send(nickname.text, password.text, Verificate));
+	}
+
+
+
+	private void Verificate(bool isVerified)
+	{
+		loadingWindow.SetActive(false);
+
+	}
+}
