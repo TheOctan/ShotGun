a1a359a93de88c4c6f43d84d52df09501d19b9cb
TheOctan <kanatush.tim@gmail.com>
2021-03-16 19:55:58 +0300

    move command

---
+++ Assets/Scripts/Entities/Commands/Command.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Entities.Commands
+{
+	public class Command : MonoBehaviour
+	{
+		public virtual void Execute()
+		{
+
+		}
+	}
+}
---
+++ Assets/Scripts/Entities/Commands/IMoveInput.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Entities.Commands
+{
+	public interface IMoveInput
+	{
+		Vector3 MoveDirection { get; }
+	}
+}
