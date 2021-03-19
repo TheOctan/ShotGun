49d60a7f91fa93704a2b8900145e1233101ecb2c
TheOctan <kanatush.tim@gmail.com>
2021-03-19 23:04:45 +0300

    physics poligon prepared

---
+++ Assets/Scripts/UI/Billboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Billboard : MonoBehaviour
+{
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        transform.LookAt(transform.position + cam.transform.forward);
+    }
+}
