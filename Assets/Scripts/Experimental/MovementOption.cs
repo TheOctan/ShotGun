aa6502fefb5802fe8035208871b771059a03a2cf
TheOctan <kanatush.tim@gmail.com>
2021-03-23 00:28:51 +0300

    testing movement and rotation

---
+++ Assets/Scripts/Experimental/MovementOption.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MovementType
+{
+	[InspectorName("Transform SetPosition()")]
+	SetPosition,
+	[InspectorName("Transform Translate()")]
+	Translate,
+	[InspectorName("Rigidbody AddForse()")]
+	RigidbodyAddForse,
+	[InspectorName("Rigidbody MovePosition()")]
+	RigidbodyMovePosition,
+	[InspectorName("Rigibody SetVelocity()")]
+	RigidbodySetVelocity
+}
+
+[RequireComponent(typeof(Rigidbody))]
+public class MovementOption : MonoBehaviour
+{
+	[SerializeField] private float speed = 5f;
+	[SerializeField] private MovementType movementType;
+
+	private Rigidbody rigidbodyComponent;
+
+	private void Awake()
+	{
+		rigidbodyComponent = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+	{
+		switch (movementType)
+		{
+			case MovementType.SetPosition:
+				SetPosition();
+				break;
+			case MovementType.Translate:
+				Translate();
+				break;
+			case MovementType.RigidbodyAddForse:
+				AddForce();
+				break;
+			case MovementType.RigidbodySetVelocity:
+				SetVelocity();
+				break;
+			case MovementType.RigidbodyMovePosition:
+				MovePosition();
+				break;
+			default:
+				break;
+		}
+	}
+	private void SetPosition()
+	{
+		transform.position += transform.forward * speed * Time.fixedDeltaTime;
+	}
+
+	private void Translate()
+	{
+		transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime);
+	}
+
+	private void AddForce()
+	{
+		rigidbodyComponent.AddForce(transform.forward * speed);
+	}
+
+	private void SetVelocity()
+	{
+		rigidbodyComponent.velocity = transform.forward * speed;
+	}
+
+	private void MovePosition()
+	{
+		rigidbodyComponent.MovePosition(rigidbodyComponent.position + transform.forward * speed * Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/Scripts/Experimental/RotationOption.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RotationType
+{
+    Transform,
+	TransformQuaternion,
+    Rigidbody
+}
+
+[RequireComponent(typeof(Rigidbody))]
+public class RotationOption : MonoBehaviour
+{
+    [SerializeField] private float turnSpeed = 3f;
+    [SerializeField] private RotationType rotationType;
+
+    private Rigidbody rigidbodyComponent;
+
+    private void Awake()
+    {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+		switch (rotationType)
+		{
+			case RotationType.Transform:
+				TransformRotate();
+				break;
+			case RotationType.TransformQuaternion:
+				TransformQuaternoinRotate();
+				break;
+			case RotationType.Rigidbody:
+				RigigidbodyRotate();
+				break;
+			default:
+				break;
+		}
+	}
+
+    private void TransformRotate()
+	{
+		transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
+	}
+
+	private void TransformQuaternoinRotate()
+	{
+
+	}
+
+	private void RigigidbodyRotate()
+	{
+		
+	}
+}
