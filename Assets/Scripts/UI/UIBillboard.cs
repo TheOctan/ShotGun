0f93109970a3c04124ca690f8976c9985f239f8d
TheOctan <kanatush.tim@gmail.com>
2021-03-29 23:19:58 +0300

    UI billboard component

---
+++ Assets/Scripts/UI/UIBillboard.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class UIBillboard : MonoBehaviour
+{
+	public bool flipVerticalOrientation = true;
+
+	private Transform cameraTransform;
+
+	private void OnEnable()
+	{
+		cameraTransform = Camera.main.transform;
+	}
+
+	private void OnDisable()
+	{
+		cameraTransform = null;
+	}
+
+	private void LateUpdate()
+	{
+		if (cameraTransform)
+		{
+			Vector3 lookDirection = transform.position + cameraTransform.forward;
+			if (flipVerticalOrientation)
+			{
+				transform.LookAt(lookDirection, cameraTransform.up);
+			}
+			else
+			{
+				transform.LookAt(lookDirection);
+			}
+		}
+	}
+}
