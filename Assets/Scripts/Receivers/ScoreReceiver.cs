1be78a935d046a04457b9c563b3d1748a56a8211
TheOctan <kanatush.tim@gmail.com>
2021-04-19 14:16:48 +0300

    save system

---
+++ Assets/Scripts/Receivers/ScoreReceiver.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Data;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreReceiver : BaseScoreReceiver
+{
+	public override IEnumerator GetScore(int minCount, Action<IEnumerable<ScoreData>> callback)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override IEnumerator GetScore(string nickname, SessionData session, int minCount, Action<IEnumerable<ScoreData>> callback)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -0,0 +1,56 @@
+﻿using SaveSystems.Serialization;
+using SaveSystems.Serialization.Format;
+using System.IO;
+using UnityEngine;
+
+namespace SaveSystems
+{
+	public class SaveSystem
+	{
+		private static ISerializationFileSystem serializationFileSystem = new BinarySerializationSystem(Application.dataPath + "/Saves/");
+
+		public static bool Save<T>(string saveName, T saveData)
+		{
+			return serializationFileSystem.SerializeObject(saveData, saveName);
+		}
+		public static T Load<T>(string path)
+		{
+			try
+			{
+				return serializationFileSystem.DeserializeObject<T>(path);
+			}
+			catch
+			{
+				Debug.LogError($"Failed to load file at {path}");
+				return default;
+			}
+		}
+		public static bool HasKey(string key)
+		{
+			return File.Exists(serializationFileSystem.DirectoryName + key + serializationFileSystem.Extension);
+		}
+		public static bool ResetSave(string key)
+		{
+			var file = new FileInfo(serializationFileSystem + key + serializationFileSystem.Extension);
+			file.Delete();
+
+			return true;
+		}
+		public static bool ResetAllSaves()
+		{
+			var directory = new DirectoryInfo(serializationFileSystem.DirectoryName);
+			directory.Delete();
+			Directory.CreateDirectory(serializationFileSystem.DirectoryName);
+
+			return true;
+		}
+		public static void SetSerializationSystem(ISerializationFileSystem serializationFileSystem)
+		{
+			SaveSystem.serializationFileSystem = serializationFileSystem;
+		}
+		public static void SetSaveDirectory(string directory)
+		{
+			serializationFileSystem.DirectoryName = directory;
+		}
+	}
+}
