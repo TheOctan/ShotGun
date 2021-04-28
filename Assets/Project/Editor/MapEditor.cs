1c8f83bde8d81b3fd63881c96db1fc9cba7c516e
TheOctan <kanatush.tim@gmail.com>
2021-04-28 22:43:30 +0300

    new project structure

---
+++ Assets/Project/Editor/MapEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapEditor : Editor
+{
+    private MapGenerator map;
+
+	private void OnEnable()
+	{
+        map = target as MapGenerator;
+    }
+
+	public override void OnInspectorGUI()
+    {
+        if (DrawDefaultInspector())
+        {
+            map.GenerateMap();
+        }
+
+        if(GUILayout.Button("Generate Map"))
+        {
+            map.GenerateMap();
+        }
+    }
+}
