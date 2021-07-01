f3750e1434f233b205e4e9d983df67bd6e41bbef
TheOctan <kanatush.tim@gmail.com>
2021-07-01 15:53:39 +0300

    rename folder to so architecture

---
+++ Assets/Sandbox/OctanGames/SOArchitecture/Scripts/Test.cs
@@ -0,0 +1,22 @@
+using ScriptableObjectArchitecture;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OctanGames
+{
+    public class Test : MonoBehaviour
+    {
+        public IntReference intRef;
+
+        private void Start()
+        {
+        
+        }
+
+        private void Update()
+        {
+        
+        }
+    }
+}
