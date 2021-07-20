bd71269b319efa43ef0ce9db1f799208d22c3420
TheOctan <kanatush.tim@gmail.com>
2021-07-20 15:12:42 +0300

    shader tutorial init

---
+++ Assets/Sandbox/OctanGames/ShaderTests/Scripts/ShaderAccessScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace OctanGames
+{
+    public class ShaderAccessScript : MonoBehaviour
+    {
+    	private void Start()
+    	{
+	        
+        }
+
+    	private void Update()
+    	{
+			
+    	}
+    }
+}
