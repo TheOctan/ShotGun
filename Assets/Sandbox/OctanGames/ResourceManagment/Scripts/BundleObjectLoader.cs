23f93166412f5e73013a08faedb7f2d748132389
TheOctan <kanatush.tim@gmail.com>
2021-07-24 22:57:11 +0300

    local loading asset bundle

---
+++ Assets/Sandbox/OctanGames/ResourceManagment/Scripts/BundleObjectLoader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace OctanGames
+{
+	public class BundleObjectLoader : MonoBehaviour
+	{
+		[SerializeField] private string assetName = "BundledSpriteObject";
+		[SerializeField] private string bundleName = "testbundle";
+
+		void Start()
+		{
+			AssetBundle localAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, bundleName));
+			if (localAssetBundle == null)
+			{
+				Debug.Log("Failed to load AssetBundle!");
+				return;
+			}
+			GameObject prefab = localAssetBundle.LoadAsset<GameObject>(assetName);
+			Instantiate(prefab);
+			localAssetBundle.Unload(false);
+		}
+	}
+}
